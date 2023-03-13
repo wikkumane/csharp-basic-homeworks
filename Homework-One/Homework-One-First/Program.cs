d90feca374c65a50c176b3db18c62155e356438d
Luka <lukesokolovski@gmail.com>
2023-03-13 19:53:59 +0100

    hmwk

---
+++ Homework-One/Homework-One-First/Program.cs
@@ -0,0 +1,17 @@
+﻿//Average Number 
+
+Console.WriteLine("Enter your first number");
+int firstNum = Convert.ToInt32(Console.ReadLine()); 
+
+Console.WriteLine("Enter second number");
+int secondNum = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter third number");
+int thirdNum = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter fourth number");
+int fourthNum = Convert.ToInt32(Console.ReadLine());    
+
+decimal avgNumber = (firstNum + secondNum + thirdNum + fourthNum) / 4;
+Console.WriteLine($"The average of {firstNum}, {secondNum}, {thirdNum}, {fourthNum} is {avgNumber}");
+
--- Homework-One/Homework-One/Program.cs
+++ Homework-One/Homework-One/Program.cs
@@ -1,2 +1,31 @@
-﻿Console.WriteLine("Hello, World!");
+﻿//Real Calculator
 
+Console.WriteLine("Enter your first number");
+int firstNum = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter your operand(- + / *)");
+string operand = Convert.ToString(Console.ReadLine());
+
+Console.WriteLine("Enter your second number");
+int secondNum = Convert.ToInt32(Console.ReadLine());
+
+if (operand == "+")
+{
+    Console.WriteLine($"The result is {firstNum + secondNum}");
+}
+else if (operand == "-")
+{
+    Console.WriteLine($"The result is {firstNum - secondNum}");
+}
+else if (operand == "*")
+{
+    Console.WriteLine($"The result is {firstNum * secondNum}");
+}
+else if (operand == "/")
+{
+    Console.WriteLine($"The result is {firstNum / secondNum}");
+}
+else
+{
+    Console.WriteLine("Wrong input");    
+}
