b9885d75937f83c57a01ff6e3641b0d2da1104a6
Luka <lukesokolovski@gmail.com>
2023-03-15 18:00:49 +0100

    t

---
+++ Homework-Two-Task2/Program.cs
@@ -0,0 +1,23 @@
+﻿string[] studentsG1 = { "Zdravko", "Petko", "Stanko", "Branko", "Trajko" };
+string[] studentsG2 = { "Josko", "Josip", "Luka", "Stojan", "Kiril"};
+Console.WriteLine("Enter 1 to show students of G1, enter 2 to show students of G2");
+int input = Convert.ToInt32(Console.ReadLine());
+
+if (input == 1)
+{
+    Console.WriteLine("The students of G1 are:");
+        foreach(string student in studentsG1)
+    {
+        Console.WriteLine(student);
+    }
+} else if  (input == 2)
+{
+    Console.WriteLine("The students of G2 are:");
+            foreach(string student in studentsG2) 
+        {  
+            Console.WriteLine(student); 
+        }
+} else
+{
+    Console.WriteLine("Wrong input");
+}
---
+++ Homework-Two/Program.cs
@@ -0,0 +1,12 @@
+﻿//Swap Numbers
+
+Console.WriteLine("Enter first number");
+int firstNum = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter second number");
+int secondNum = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Before swap first number: {firstNum}, second number: {secondNum}");
+int newNum = firstNum;
+firstNum = secondNum;
+secondNum = newNum;
+Console.WriteLine($"After swap first number: {firstNum}, second number: {secondNum}");
