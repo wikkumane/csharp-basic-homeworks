84498dd04a3b8d8029918f1e35655a65bf695c60
wikkumane <lukesokolovski@gmail.com>
2023-03-23 17:50:24 +0100

    hmwk

---
+++ Homework-Four/Program.cs
@@ -0,0 +1,23 @@
+﻿//Task 1 
+
+Console.WriteLine("Enter a sentence longer than 5 characters, this will print the last 5 letters of the sentence");
+string InputSentence = Console.ReadLine();  
+
+if (InputSentence.Length < 5)
+{
+    Console.WriteLine("Enter a sentence longer than 5 characters!");
+} else
+{
+    string LastFive = InputSentence.Substring(InputSentence.Length - 5);
+    Console.WriteLine(LastFive);
+}
+
+//Task 2 
+
+Console.WriteLine("Enter any sentence, this will split the sentence with a space between every letter");
+string InputSentenceTwo = Console.ReadLine();
+
+string[] TaskTwoSplit = InputSentenceTwo.Split(" ");
+
+// Task 3 
+
---
+++ Homework-Three/Program.cs
@@ -0,0 +1,45 @@
+﻿int multiply (int FirstNum, int SecondNum) { return FirstNum * SecondNum; }
+
+int divide(int FirstNum, int SecondNum) { return FirstNum / SecondNum; }
+
+void MainCalculator()
+{
+    Console.WriteLine("Enter your first number");
+    int FirstNumInput = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Enter your desired operator");
+    string inputOperator = Console.ReadLine();
+
+    Console.WriteLine("Enter your second number");
+    int SecondNumInput = Convert.ToInt32(Console.ReadLine());
+
+    switch (inputOperator)
+    {
+        case "*":
+            Console.WriteLine($"{multiply(FirstNumInput, SecondNumInput)}");
+            break;
+
+        case "/":
+            if (SecondNumInput == 0)
+            {
+
+                Console.WriteLine("You cannot divide with zero");
+
+            }
+            else
+
+            {
+
+                Console.WriteLine($"{divide(FirstNumInput, SecondNumInput)}");
+            }
+
+            break;
+
+
+    }
+
+
+
+}
+
+MainCalculator();
